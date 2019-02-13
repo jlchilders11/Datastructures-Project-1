8c997b6f8604ae8880e5e051b3f37a1399375905
Jeremy Childers <jlchilders11@k2m.com>
2019-02-13 00:17:31 -0500

    initial

---
+++ Data Structures Project 1/Data Structures Project 1.cpp
@@ -0,0 +1,59 @@
+﻿// Data Structures Project 1.cpp : Defines the entry point for the application.
+//
+
+#include "Data Structures Project 1.h"
+
+int main()
+{
+	std::string graph_name;
+	std::cout << "Please enter your data file name: ";
+	std::cin >> graph_name;
+
+	std::ifstream graph_file (graph_name);
+
+	if (graph_file.is_open())
+	{
+		std::vector<std::list<int>> adjList;
+		std::string str;
+		std::list<int> mtLst;  // an empty list
+
+		for (int i = 0; i < 4; ++i)
+		{
+			adjList.push_back(mtLst); // push an empty list onto the adjList
+			for (int j = 0; j <= i; ++j)
+				adjList[i].push_back(j);  // populate the list just pushed onto the vector
+
+		}
+
+		printAdjList(adjList);
+
+		std::string terminator = "0";
+		while (terminator != "-1")
+		{
+			std::cout << "Enter graphs to merge, or -1 to exit: ";
+			std::cin >> terminator;
+		}
+	}
+	else
+	{
+		std::cout << "That file does not exist. ";
+		std::system("pause");
+	}
+
+	
+	return 0;
+}
+
+//print out passed vector, by line with line identified by list + position
+void printAdjList(std::vector<std::list<int>> &path)
+{
+	for (auto i = path.begin(); i != path.end(); ++i)
+	{
+		std::cout << "list" << i - path.begin() << " ";
+		for (auto it = i->begin(); it != i->end(); ++it)
+		{
+			std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+	}
+}
---
+++ Data Structures Project 1/Data Structures Project 1.h
@@ -0,0 +1,15 @@
+﻿// Data Structures Project 1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <vector>
+#include <list>
+
+//prototypes
+void printAdjList(std::vector<std::list<int>> &path);
+std::list<int>::iterator  find_gt(std::list<int>::iterator start, std::list<int>::iterator stop, int x);
